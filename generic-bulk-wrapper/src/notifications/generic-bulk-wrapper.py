a8ed0c74f4834e6f936f6ea795fd04bd23251df1
Stefan Mayr <stefan@mayr-stefan.de>
2025-05-15 00:04:50 +0200

    Make linter happy

--- generic-bulk-wrapper/src/notifications/generic-bulk-wrapper.py
+++ generic-bulk-wrapper/src/notifications/generic-bulk-wrapper.py
@@ -5,9 +5,9 @@
 import os
 import sys
 import subprocess
+from typing import NoReturn
 from cmk.utils.paths import omd_root
 from cmk.notification_plugins import utils
-from typing import NoReturn
 
 opt_debug = "-d" in sys.argv
 bulk_mode = "--bulk" in sys.argv
@@ -19,13 +19,13 @@ def call_script(script_env: dict[str, str]) -> NoReturn:
     # check if a script name was given or exit
     if 'NOTIFY_PARAMETER_1' not in script_env:
         sys.stderr.write('First parameter must be the notification script to call: parameter is empty')
-        exit(1)
+        sys.exit(1)
 
     # because we are a wrapper we have to shift parameters:
     #   NOTIFY_PARAMETER_1 -> script to call
     #   NOTIFY_PARAMETER_2 -> NOTIFY_PARAMETER_1
     #   NOTIFY_PARAMETER_n+1 -> NOTIFY_PARAMETER_n
-    script_name = script_env['NOTIFY_PARAMETER_1'];
+    script_name = script_env['NOTIFY_PARAMETER_1']
     param_count = len(list(filter(lambda k: k.startswith('NOTIFY_PARAMETER_'), script_env)))
     # shift all parameters one position
     for i in range(1, param_count):
@@ -38,9 +38,9 @@ def call_script(script_env: dict[str, str]) -> NoReturn:
     # call script with processes environment
     if opt_debug:
         sys.stderr.write(f'call {script_name} with env: { script_env}\n')
-    process = subprocess.run(script_name, env=script_env)
+    process = subprocess.run(script_name, env=script_env, check=False)
     if not bulk_mode:
-        exit(process.returncode)
+        sys.exit(process.returncode)
 
 if __name__ == "__main__":
     if opt_debug:
