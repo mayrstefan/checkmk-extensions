8e65e051df9898958e0f4f6e88ce98b576fe8954
Stefan Mayr <stefan@mayr-stefan.de>
2025-11-02 23:25:21 +0100

    Initial version for Valkey check

---
+++ valkey/src/cmk_addons_plugins/valkey/rulesets/valkey_info.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# mypy: disable-error-code="no-untyped-def"
+
+from cmk.rulesets.v1 import Title
+from cmk.rulesets.v1.form_specs import (
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    LevelDirection,
+    migrate_to_float_simple_levels,
+    SimpleLevels,
+    SingleChoice,
+    SingleChoiceElement,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import CheckParameters, HostAndItemCondition, Topic
+
+
+def _parameter_form_valkey_info():
+    return Dictionary(
+        elements={
+            "expected_mode": DictElement(
+                parameter_form=SingleChoice(
+                    title=Title("Expected mode"),
+                    elements=[
+                        SingleChoiceElement("standalone", Title("Standalone")),
+                        SingleChoiceElement("sentinel", Title("Sentinel")),
+                        SingleChoiceElement("cluster", Title("Cluster")),
+                    ],
+                ),
+            ),
+            "min": DictElement(
+                parameter_form=SimpleLevels(
+                    level_direction=LevelDirection.LOWER,
+                    title=Title("Minimum allowed uptime"),
+                    form_spec_template=TimeSpan(
+                        displayed_magnitudes=[
+                            TimeMagnitude.DAY,
+                            TimeMagnitude.HOUR,
+                            TimeMagnitude.MINUTE,
+                            TimeMagnitude.SECOND,
+                        ]
+                    ),
+                    prefill_fixed_levels=DefaultValue(value=(0.0, 0.0)),
+                    migrate=migrate_to_float_simple_levels,
+                ),
+            ),
+            "max": DictElement(
+                parameter_form=SimpleLevels(
+                    level_direction=LevelDirection.UPPER,
+                    title=Title("Maximum allowed uptime"),
+                    form_spec_template=TimeSpan(
+                        displayed_magnitudes=[
+                            TimeMagnitude.DAY,
+                            TimeMagnitude.HOUR,
+                            TimeMagnitude.MINUTE,
+                            TimeMagnitude.SECOND,
+                        ]
+                    ),
+                    prefill_fixed_levels=DefaultValue(value=(0.0, 0.0)),
+                    migrate=migrate_to_float_simple_levels,
+                ),
+            ),
+        }
+    )
+
+
+rule_spec_valkey_info = CheckParameters(
+    name="valkey_info",
+    title=Title("Valkey info"),
+    topic=Topic.APPLICATIONS,
+    parameter_form=_parameter_form_valkey_info,
+    condition=HostAndItemCondition(item_title=Title("Valkey server name")),
+)
